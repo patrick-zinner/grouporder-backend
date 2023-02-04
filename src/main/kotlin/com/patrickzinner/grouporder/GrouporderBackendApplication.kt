f0571b72700611bb31ddecaed2a4c508ea46a746
Patrick Zinner <patrick.zinner@viesure.io>
2023-02-04 15:13:40 +0100

    first commit

---
+++ src/main/kotlin/com/patrickzinner/grouporder/GrouporderBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.patrickzinner.grouporder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrouporderBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<GrouporderBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/patrickzinner/grouporder/GrouporderBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.patrickzinner.grouporder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrouporderBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
